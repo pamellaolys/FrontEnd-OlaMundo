9e307f7ab054eb1caf9a49a14c27efa0c0cd4584
Pamella Santos <pamellaolys@gmail.com>
2021-07-28 15:50:39 -0300

    task1 - menu/rodape/quemsomos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { QuemsomosComponent } from './quemsomos/quemsomos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    QuemsomosComponent
   ],
   imports: [
     BrowserModule,
